66e2113774abe8058cc0583b9525013885f524ad
kamooni <tjrghks96@naver.com>
2021-01-11 07:39:34 +0900

    #11.7 ~ 11.8 3번째 방법 class based view + class based view에서의 context 방법

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,19 @@
-from math import ceil
-from datetime import datetime
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+
+    # object_list 이름 변경
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
