822172e76a499e43c6c0b54b919f1b844d02f247
kamooni <tjrghks96@naver.com>
2021-01-11 01:02:03 +0900

    #10.3 base.html & extends template page

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from django.shortcuts import render  # response에 html을 넣어서 전달할 수 있게 해준다 !
+from . import models
 
 
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
